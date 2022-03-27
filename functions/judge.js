7363a8d620dcdc805c9a72940bd9fbd4bb0ac72f
elenaclare <elena@clare.ai>
2022-03-27 15:54:05 +0300

    initial stable build

---
+++ functions/judge.js
@@ -0,0 +1,62 @@
+require('dotenv').config();
+
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+
+exports.handler = async (event) => {
+  const doc = new GoogleSpreadsheet(process.env.GOOGLE_SPREADSHEET_ID);
+
+  try {
+    await doc.useServiceAccountAuth({
+      client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    });
+
+    await doc.loadInfo();
+
+    const sheet = doc.sheetsByIndex[0];
+
+    const data = JSON.parse(event.body);
+
+    const rows = await sheet.getRows();
+
+    // if (rows.some((row) => row.email === data.email)) {
+    //   // Формируем ответ
+    //   const response = {
+    //     statusCode: 400,
+    //     body: JSON.stringify({
+    //       error: 'Пользователь с таким email уже оформил подписку',
+    //     }),
+    //     // Про это поговорим позже
+    //     headers: {
+    //       'Access-Control-Allow-Origin': '*',
+    //       'Access-Control-Allow-Credentials': 'true',
+    //     },
+    //   };
+    //   // и возвращаем его
+    //   return response;
+    // }
+
+    await sheet.addRow(data);
+
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Записано' }),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': 'true',
+      },
+    };
+    return response;
+  } catch (err) {
+    console.error(err);
+    const response = {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Что-то пошло не так. Попробуйте позже' }),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': 'true',
+      },
+    };
+    return response;
+  }
+};
