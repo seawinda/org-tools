68df00307486593d2636d3abd6952b759507cff3
elenaclare <elena@clare.ai>
2022-04-02 22:12:22 +0300

    theme  provider

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from './designTokens';
+
+const theme = defaultTheme;
 
 ReactDOM.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}></ThemeProvider>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
